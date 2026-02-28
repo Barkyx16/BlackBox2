ef062b76ef6cea7a91b79ece27b99525bcbf961d
Alex Rafalski <your_email@example.com>
2026-02-28 01:20:23 -0700

    Refactor: extract output logic from main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,11 @@ public class Main {
         RacecarExample racecar = new RacecarExample();
         int avgSpeed = 100;
         boolean result = racecar.willFinish(avgSpeed);
+
+        displayResult(result);
+    }
+
+    private static void displayResult(boolean result) {
         System.out.println("return: " + result);
     }
 }
